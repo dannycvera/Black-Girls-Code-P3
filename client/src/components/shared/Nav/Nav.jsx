76a6cdc84449199890ee989e645ac863fc654b7c
danv <dannyvera@gmail.com>
2020-08-22 17:50:45 -0400

    nav bar drop down fade and links added

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -18,7 +18,6 @@ function Nav() {
               <li>
                 <NavLink to="/">What We Do</NavLink>
               </li>
-
               <li>
                 <NavLink to="/whereyourmoneygoes">
                   Where Your Money Goes
@@ -26,22 +25,55 @@ function Nav() {
               </li>
             </ul>
           </div>
-
-          <NavLink className="link" to="/">
-            PROGRAM/EVENTS
-          </NavLink>
-          <NavLink className="link" to="/">
-            PRESS
-          </NavLink>
-          <NavLink className="link" to="/">
-            JOIN US
-          </NavLink>
+          <div className="drop-down-list">
+            <NavLink className="link" to="/">
+              PROGRAM/EVENTS
+            </NavLink>
+            <ul className="drop-down">
+              <li>
+                <NavLink to="/">BCG Hackathons</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="drop-down-list">
+            <NavLink className="link" to="/">
+              PRESS
+            </NavLink>
+            <ul className="drop-down">
+              <li>
+                <NavLink to="/">Press Only</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="drop-down-list">
+            <NavLink className="link" to="/">
+              JOIN US
+            </NavLink>
+            <ul className="drop-down">
+              <li>
+                <NavLink to="/">Career Openings</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Volunteer Signup</NavLink>
+              </li>
+            </ul>
+          </div>
           <NavLink className="link" to="/">
             PARENTS
           </NavLink>
-          <NavLink className="link" to="/">
-            CONTACT US
-          </NavLink>
+          <div className="drop-down-list">
+            <NavLink className="link" to="/">
+              CONTACT US
+            </NavLink>
+            <ul className="drop-down">
+              <li>
+                <NavLink to="/">Mailing List</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Donations</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
