df1d4ad19565b090a00456ac369a4aaca50a5e11
Anna Schneider <anna.schneiderm@gmail.com>
2020-08-22 11:44:45 -0400

    removed header from nav

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,66 +1,62 @@
-import React, { useState } from "react";
-import "./Nav.css";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react"
+import "./Nav.css"
+import { NavLink } from "react-router-dom"
 
 function Nav() {
-  const [show, updShow] = useState("");
+	const [show, updShow] = useState("")
 
-  const handleHover = () => {
-    updShow("show");
-  };
+	const handleHover = () => {
+		updShow("show")
+	}
 
-  const handleHoverExit = () => {
-    updShow("");
-  };
-  return (
-    <nav>
-      <div className="nav">
-        <div className="header">
-          <NavLink className="logo" to="/">
-            EventsApp
-          </NavLink>
-          <div>
-            <input type="text"></input>
-          </div>
-        </div>
-        <div className="links">
-          <NavLink className="link" to="/">
-            HOME
-          </NavLink>
-          <div className="drop-down-about">
-            <NavLink
-              className="link"
-              to="/"
-              onMouseEnter={handleHover}
-              onMouseLeave={handleHoverExit}
-            >
-              ABOUT BGC
-            </NavLink>
-            <ul className={`drop-down ${show}`}>
-              <li>What We Do</li>
-              <li>Where Your Money Goes</li>
-            </ul>
-          </div>
+	const handleHoverExit = () => {
+		updShow("")
+	}
+	return (
+		<nav>
+			<div className="nav">
+				<div>
+					<input type="text"></input>
+				</div>
 
-          <NavLink className="link" to="/">
-            PROGRAM/EVENTS
-          </NavLink>
-          <NavLink className="link" to="/">
-            PRESS
-          </NavLink>
-          <NavLink className="link" to="/">
-            JOIN US
-          </NavLink>
-          <NavLink className="link" to="/">
-            PARENTS
-          </NavLink>
-          <NavLink className="link" to="/">
-            CONTACT US
-          </NavLink>
-        </div>
-      </div>
-    </nav>
-  );
+				<div className="links">
+					<NavLink className="link" to="/">
+						HOME
+					</NavLink>
+					<div className="drop-down-about">
+						<NavLink
+							className="link"
+							to="/"
+							onMouseEnter={handleHover}
+							onMouseLeave={handleHoverExit}
+						>
+							ABOUT BGC
+						</NavLink>
+						<ul className={`drop-down ${show}`}>
+							<li>What We Do</li>
+							<li>Where Your Money Goes</li>
+						</ul>
+					</div>
+
+					<NavLink className="link" to="/">
+						PROGRAM/EVENTS
+					</NavLink>
+					<NavLink className="link" to="/">
+						PRESS
+					</NavLink>
+					<NavLink className="link" to="/">
+						JOIN US
+					</NavLink>
+					<NavLink className="link" to="/">
+						PARENTS
+					</NavLink>
+					<NavLink className="link" to="/">
+						CONTACT US
+					</NavLink>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
-export default Nav;
+export default Nav
