33c16ca54291b3027a3580573eeafaa0dc4feb38
Anna Schneider <anna.schneiderm@gmail.com>
2020-08-22 11:38:29 -0400

    Added/Styled logo with link

--- client/src/components/shared/Layout/Layout.jsx
+++ client/src/components/shared/Layout/Layout.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import "./Layout.css";
-import Nav from "../Nav/Nav";
-import Footer from "../Footer/Footer";
+import React from "react"
+import "./Layout.css"
+import Logo from "../Logo/Logo"
+import Nav from "../Nav/Nav"
+import Footer from "../Footer/Footer"
 
 function Layout(props) {
-  return (
-    <div className="layout">
-      <Nav />
-      <div className="layout-children">{props.children}</div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="layout">
+			<Logo />
+			<Nav />
+			<div className="layout-children">{props.children}</div>
+			<Footer />
+		</div>
+	)
 }
 
-export default Layout;
+export default Layout
