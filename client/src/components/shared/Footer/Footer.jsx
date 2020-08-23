6fe3de1b74d20f939999e5bfd7db7130fdef4c39
Anna Schneider <anna.schneiderm@gmail.com>
2020-08-22 23:42:24 -0400

    In progress

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import "./Footer.css";
+import React from "react"
+import "./Footer.css"
 
 function Footer() {
-  return <div></div>;
+	return (
+		<div className="footer">
+			<h3>Follow Us</h3>
+		</div>
+	)
 }
 
-export default Footer;
+export default Footer
---
+++ client/src/components/shared/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Search.css"
+
+export default function Search() {
+	return (
+		<div className="container">
+			<div className="social">social placeholder</div>
+			<input type="text" className="search" placeholder="Search"></input>
+			<button>DONATE</button>
+		</div>
+	)
+}
