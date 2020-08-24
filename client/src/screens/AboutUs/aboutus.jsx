f59e6f8d94dbec5be29dfdcf6385b9fa8eac231b
rickyransom <rickyransomco@gmail.com>
2020-08-24 07:49:04 -0400

    abousus

--- client/src/screens/AboutUs/aboutus.jsx
+++ client/src/screens/AboutUs/aboutus.jsx
@@ -3,7 +3,7 @@ import "./aboutus.css";
 import Layout from "../../components/shared/Layout/Layout";
 import Footer from "client/src/components/shared/Footer/Footer.jsx"
 import Nav "from client/src/components/shared/Nav/Nav.jsx"
-
+import React-DOM
 //flex box component testing
 const ContainerStyle = {
   display: 'flex',
