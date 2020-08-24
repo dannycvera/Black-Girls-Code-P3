d079ddf4571c97e46ef1cfa43e419c05eddc2fee
Anna Schneider <anna.schneiderm@gmail.com>
2020-08-24 09:28:53 -0400

    imported social icons

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from "react"
 import "./Footer.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import {
+// 	faFacebook,
+// 	faTwitter,
+// 	faLinkedIn,
+// } from "@fortawesome/react-fontawesome"
+
+// library.add(
+// 	faFacebook,
+// 	faTwitter
+// 	// faLinkedIn,
+// )
 
 function Footer() {
 	return (
 		<div className="footer">
 			<h3>Follow Us</h3>
+			<div className="social-bar">
+				<a href="#" className="facebook">
+					<FontAwesomeIcon icon={["fab", "facebook"]} />
+				</a>
+				<a href="#" className="twitter">
+					<FontAwesomeIcon icon={["fab", "twitter"]} />
+				</a>
+				<a href="#" className="linkedin">
+					<FontAwesomeIcon icon={["fab", "linkedin"]} />
+				</a>
+			</div>
 		</div>
 	)
 }
