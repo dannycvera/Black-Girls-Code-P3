3e573723530beaecfc9b34760688bde781039018
rickyransom <rickyransomco@gmail.com>
2020-08-24 08:20:33 -0400

    about us css edited

--- client/src/screens/AboutUs/aboutus.jsx
+++ client/src/screens/AboutUs/aboutus.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-import "./aboutus.css";
+import  "./aboutus.css";
 import Layout from "../../components/shared/Layout/Layout";
 import Footer from "client/src/components/shared/Footer/Footer.jsx"
 import Nav "from client/src/components/shared/Nav/Nav.jsx"
-import React-DOM
+
 //flex box component testing
-const ContainerStyle = {
+/* const ContainerStyle = {
   display: 'flex',
   justifyContent: 'space-between',
-  width:
+  width: */
 }
+
+/* const Flex = (props) => (
+  <div
+    className={props.className}
+    style={{
+      display: props.container ? 'flex' : 'block',
+      justifyContent: props.justifyContent || 'flex-start',
+      flexDirection: props.flexDirection || 'row',
+      flexGrow: props.flexGrow || 0,
+      flexBasis: props.flexBasis || 'auto',
+      flexShrink: props.flexShrink || 1,
+      flexWrap: props.flexWrap || 'nowrap',
+      flex: props.flex || '0 1 auto',
+      alignItems: props.alignItems || 'stretch',
+      margin: props.margin || '0',
+      padding: props.padding || '0',
+      width: props.width || 'auto',
+      height: props.height || 'auto',
+      maxWidth: props.maxWidth || 'none'
+    }}
+  >
+    {props.children}
+  </div> */
