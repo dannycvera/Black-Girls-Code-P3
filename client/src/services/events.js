ccfe455feac039beb4e6b9e5ad9784599028f0ae
danv <dannyvera@gmail.com>
2020-08-25 18:01:02 -0400

    fixed api config and events

--- client/src/services/events.js
+++ client/src/services/events.js
@@ -1,46 +1,46 @@
-import api from "./apiConfig"
+import api from "./apiConfig";
 
 export const getEvents = async () => {
-	try {
-		const response = await api.get("/events")
-		return response.data
-	} catch (error) {
-		throw error
-	}
-}
+  try {
+    const response = await api.get("/");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const getEvent = async (id) => {
-	try {
-		const response = await api.get(`/events/${id}`)
-		return response.data
-	} catch (error) {
-		throw error
-	}
-}
+  try {
+    const response = await api.get(`/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const createEvent = async (event) => {
-	try {
-		const response = await api.post("/events", event)
-		return response.data
-	} catch (error) {
-		throw error
-	}
-}
+  try {
+    const response = await api.post("/", event);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const updateEvent = async (id, event) => {
-	try {
-		const response = await api.put(`/events/${id}`, event)
-		return response.data
-	} catch (error) {
-		throw error
-	}
-}
+  try {
+    const response = await api.put(`/${id}`, event);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const deleteEvent = async (id) => {
-	try {
-		const response = await api.delete(`/events/${id}`)
-		return response.data
-	} catch (error) {
-		throw error
-	}
-}
+  try {
+    const response = await api.delete(`/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
