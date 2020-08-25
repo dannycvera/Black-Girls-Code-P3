690e1c7384983e62170552df1e931a53f578b08a
dannycvera <dannyvera@gmail.com>
2020-08-25 16:12:46 -0400

    Revert "Feature/aboutus"

--- client/src/components/shared/Layout/Layout.jsx
+++ client/src/components/shared/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import "./Layout.css"
 import Logo from "../Logo/Logo"
 import Nav from "../Nav/Nav"
 import Search from "../Search/Search"
-// import Footer from "../Footer/Footer"
+import Footer from "../Footer/Footer"
 
 function Layout(props) {
 	return (
@@ -14,12 +14,8 @@ function Layout(props) {
 			</header>
 			<Nav />
 			<div className="layout-children">{props.children}</div>
-<<<<<<< HEAD
-			{/* <Footer /> */}
-=======
 			<div className="clear-fix"></div>
 			<Footer />
->>>>>>> cd31aa3a7c4e58f9a1c758ba53528ccce60a7ad7
 		</div>
 	)
 }
